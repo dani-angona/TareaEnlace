2cad4cd67bb677d1217643f9c88513bb80fce2e9
dani-angona <dani.angona@gmail.com>
2025-02-04 17:41:21 +0100

    hacer proyecto entero

--- TareaEnlace/TareaEnlace/MainWindow.xaml.cs
+++ TareaEnlace/TareaEnlace/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System;
+using System.IO;
+using System.Xml.Linq;
 
 namespace TareaEnlace
 {
@@ -23,6 +26,46 @@ namespace TareaEnlace
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void TextChanged_UpdateUI(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            lblNombrePreview.Text = txtNombre.Text;
+            lblTelefonoPreview.Text = txtTelefono.Text;
+
+
+            btnGuardar.IsEnabled = !string.IsNullOrWhiteSpace(txtNombre.Text) &&
+                                   !string.IsNullOrWhiteSpace(txtTelefono.Text);
+        }
+
+
+        private void GuardarEnXML(object sender, RoutedEventArgs e)
+        {
+            string nombre = txtNombre.Text;
+            string telefono = txtTelefono.Text;
+            string filePath = "archivo.xml";
+
+            XDocument doc;
+
+            // Si el archivo XML ya existe, lo cargamos; si no, lo creamos
+            if (File.Exists(filePath))
+            {
+                doc = XDocument.Load(filePath);
+            }
+            else
+            {
+                doc = new XDocument(new XElement("Tareas"));
+            }
+
+
+            XElement nuevoArchivo = new XElement("Tarea",
+                new XElement("Nombre", nombre),
+                new XElement("Telefono", telefono));
+
+            doc.Root.Add(nuevoArchivo);
+            doc.Save(filePath);
+
+            MessageBox.Show("Bien hecho", "Éxito", MessageBoxButton.OK, MessageBoxImage.Information);
         }
     }
 }
